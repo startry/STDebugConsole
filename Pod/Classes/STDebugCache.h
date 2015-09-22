8db37a1916b04a3fca249b27e4735aa8f57353c2
chenxing.cx <chenxingfl@gmail.com>
2015-09-22 18:49:01 +0800

    delete chinese chars

--- Pod/Classes/STDebugCache.h
+++ Pod/Classes/STDebugCache.h
@@ -3,7 +3,7 @@
 //  STDebugConsoleExample
 //
 //  Created by chenxing.cx on 15/9/22.
-//  Copyright © 2015年 Startry. All rights reserved.
+//  Copyright © 2015 Startry. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
