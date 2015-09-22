ef98f552ca8ba3bb1583abd706e5bc4224fb64dd
chenxing.cx <chenxingfl@gmail.com>
2015-09-22 20:53:42 +0800

    modify test case

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -28,7 +28,8 @@
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertEqual(1, 1);
+//    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
